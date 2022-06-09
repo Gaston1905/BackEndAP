fab4459f4428f4d977d8ad835123060017b302ca
Gaston Gaitan <80048998+Gaston1905@users.noreply.github.com>
2022-06-08 21:30:59 -0300

    Update UserRepositoryTest.java

--- BackAP-main/src/test/java/UserRepositoryTest.java
+++ BackAP-main/src/test/java/UserRepositoryTest.java
@@ -1,22 +1,2 @@
 
-import com.gaitan.SpringAuthentication.domain.User;
-import com.gaitan.SpringAuthentication.repository.UserRepository;
-import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.annotation.Rollback;
-
-
-
-
-
-
-
-
-
-    
-}
